9e5b30760a362316f9f5a2d624086b7b88f045c4
MinZexing <zexing1908@gmail.com>
2025-11-23 00:26:57 +1100

    add activity controller mediator for better structure

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using MediatR;
 
 namespace API.Controllers
 {
@@ -8,5 +9,8 @@ namespace API.Controllers
     [ApiController]
     public class BaseApiController: ControllerBase
     {
+        private IMediator? _mediator;
+
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>()?? throw new InvalidOperationException("IMediator service is unavailable");
     }
 }
