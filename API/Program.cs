3170b47da9f409ed207ecc5c88fe81192bec582d
MinZexing <zexing1908@gmail.com>
2025-12-02 19:47:12 +1100

    configure and add client frontend to the repo

--- API/Program.cs
+++ API/Program.cs
@@ -20,6 +20,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
---
+++ client/src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    fetch("https://localhost:5001/api/activities")
+      .then((response) => response.json())
+      .then((data) => setActivities(data));
+
+    return () => {};
+  }, []);
+
+  return (
+    <>
+      <Typography variant="h3">Reactivities</Typography>
+      <List>
+        {activities.map((activity) => (
+          <ListItem key={activity.id}>
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default App;
