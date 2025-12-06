ed9fe90dfff01f4a9480c0d85a1c9c3aa869e26c
MinZexing <zexing1908@gmail.com>
2025-12-06 19:18:49 +1100

    add loading indicator in mobx

--- client/src/lib/stores/counterStore.ts
+++ client/src/lib/stores/counterStore.ts
@@ -1,13 +1,25 @@
-import { makeObservable, observable } from "mobx";
+import { makeAutoObservable } from "mobx";
 
 export default class CounterStore {
   title = "Counter store";
-  count = 0;
+  count = 42;
+  events: string[] = [`Initial count is ${this.count}`];
 
   constructor() {
-    makeObservable(this, {
-      title: observable,
-      count: observable,
-    });
+    makeAutoObservable(this);
+  }
+
+  increment = (amount = 1) => {
+    this.count += amount;
+    this.events.push(`Incremented by ${amount} - count is now ${this.count}`);
+  };
+
+  decrement = (amount = 1) => {
+    this.count -= amount;
+    this.events.push(`Decremented by ${amount} - count is now ${this.count}`);
+  };
+
+  get eventCount() {
+    return this.events.length;
   }
 }
